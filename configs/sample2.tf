d4192c7e170c113a11a30fe96a1c4d77aaa5a983
oliaboo <budzhakolga@gmail.com>
2019-08-01 17:06:50 +0300

    add disk in 2 modules

--- configs/sample2.tf
+++ configs/sample2.tf
@@ -2,3 +2,11 @@ variable "image_id" {
   type = string
 }
 
+resource "google_compute_disk" "non-default" {
+  name  = "test-disk"
+  labels = {
+    environment = "dev"
+  }
+  image = "gce-uefi-images/windows-1803-core"
+  physical_block_size_bytes = 4096
+}
